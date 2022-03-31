f4b88d07fb8b4aed1817260daa2a9cf791914465
mukhtadirfiroz <101871653+mukhtadirfiroz@users.noreply.github.com>
2022-03-31 22:48:28 +0530

    Update details.c

--- 3_Implementation/src/details.c
+++ 3_Implementation/src/details.c
@@ -1,1 +1,15 @@
+#include "function.h"
+#include "moviedetails.c"
+#include<string.h>
 
+struct movie_details person[300];
+void details(void)
+{
+	int i;
+	int count =0;
+	char pass[10],pak[10]="admin";
+	printf("Enter the password to see details: ");
+	scanf("%s",&pass);
+	if (strcmp(pass,pak)==0)
+	{
+		for (i=0;i<count;i++)
