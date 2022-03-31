159c8754a5aa76ec1fc2dc796793e975b5222685
mukhtadirfiroz <101871653+mukhtadirfiroz@users.noreply.github.com>
2022-03-31 22:53:26 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,1 +1,28 @@
+#include "unity.h"
+#include "function.h"
+
+void setUp()
+{
+
+}
+void tearDown()
+{
+  
+}
+void test_changepriceticket(void)
+{
+    int price1=700;
+    TEST_ASSERT_EQUAL(700,changepriceticket(price1));
+    
+}
+
+int main(void)
+{
+  UNITY_BEGIN();
+  
+  RUN_T
+EST(test_changepriceticket);
+  return UNITY_END();
+}
+
 
